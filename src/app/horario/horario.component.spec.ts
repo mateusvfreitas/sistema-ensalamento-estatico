c459667e6f8d0c3819c5c967f3bfd5a27ec6b237
mateusvfreitas <mateusvfreitas@gmail.com>
2023-01-10 20:04:59 -0300

    correção de teste

--- src/app/horario/horario.component.spec.ts
+++ src/app/horario/horario.component.spec.ts
@@ -1,23 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { HorarioComponent } from './horario.component';
 
 describe('HorarioComponent', () => {
-  let component: HorarioComponent;
-  let fixture: ComponentFixture<HorarioComponent>;
+    let component: HorarioComponent;
+    let fixture: ComponentFixture<HorarioComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HorarioComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [HorarioComponent],
+            imports: [MatSnackBarModule],
+            providers: [{ provide: MatDialog, useValue: {} }],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(HorarioComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(HorarioComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
